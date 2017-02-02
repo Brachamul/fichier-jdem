4c33bfd3b6826452446d5dfe48b7a134249c886f
Brachamul <barnaby.brachamul@gmail.com>
2017-02-02 23:32:03 +0100

    installed wagtail, sitll have to make it look good

--- _project/urls.py
+++ _project/urls.py
@@ -10,5 +10,18 @@ urlpatterns = [
 	url(r'^arriere-boutique/', admin.site.urls, name='admin'),
 	url(r'^fichier/', include('fichiers_adherents.urls')),
 	url(r'^phoning/', include('phoning.urls')),
-	url(r'^$', RedirectView.as_view(url="/phoning", permanent=False)),
+#	url(r'^$', RedirectView.as_view(url="/phoning", permanent=False)),
+]
+
+
+# Pages de guide des responsables de fédérations avec Wagtail
+
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailcore import urls as wagtail_urls
+
+urlpatterns += [
+    url(r'^cms/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^$', include(wagtail_urls)),
 ]
